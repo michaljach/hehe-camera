3aa846ea11e1571bf437a07e54acd101a00b4551
Michal Jach <michaljach@gmail.com>
2026-02-05 11:27:15 +0100

    Initial Commit

---
+++ hehe/heheApp.swift
@@ -0,0 +1,17 @@
+//
+//  heheApp.swift
+//  hehe
+//
+//  Created by Michal Jach on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct heheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
